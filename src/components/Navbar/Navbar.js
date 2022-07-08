11af8a5fbf1a4a14b8c65efed4538d98792554d5
Firecry <lubiexd80@gmail.com>
2022-07-08 03:35:45 +0200

    startup

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Profile</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar;
