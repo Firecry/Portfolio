e04022e3b602234f5fe215518d9153872acf24a5
Firecry <lubiexd80@gmail.com>
2022-07-16 23:30:43 +0200

    small updates

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -3,7 +3,7 @@ import '../css/Aboutme.css'
 export default function Aboutme() {
     return(
         <div className='aboutme'>
-            <div className='description'>Hello World!</div>
+            {/* <div className='description'>Hello World!</div> */}
         </div>
     )
 }
