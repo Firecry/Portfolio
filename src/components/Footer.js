dfb53fbbb1e94d767a80ee877a4ea7b6d753f8c4
Firecry <lubiexd80@gmail.com>
2022-07-18 04:01:28 +0200

    mental breakdown

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@ import '../css/Footer.css'
 
 export default function Footer() {
     return (
-        <div className='footer'></div>
+        <div className='footer'>
+            <div className='footerTitle'>More Info</div>
+        </div>
     )
 }
