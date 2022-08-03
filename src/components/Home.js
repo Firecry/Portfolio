f033edc6a4299c33625801b1fe7bc454750ec085
Firecry <lubiexd80@gmail.com>
2022-08-03 10:57:16 +0200

    some updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import '../css/Home.css'
 export default function Home() {
     return (
         <div className='home'>
-            <div>Welcome! Im Simon</div>
+            <div>Welcome!</div>
         </div>
     )
 }
