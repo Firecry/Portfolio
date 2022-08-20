2d4375d015a8acc821f9d31958127ad18798e1e6
Firecry <lubiexd80@gmail.com>
2022-08-20 22:48:55 +0200

    updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,22 @@
 import '../css/Home.css'
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    useEffect(() => {
+        const handleNavigation = () => window.scrollY ? setIsScrolled(true) : setIsScrolled(false)
+
+        window.addEventListener('scroll', handleNavigation)
+
+        return () => {
+            window.removeEventListener('scroll', handleNavigation)
+        }
+    })
+
     return (
         <div className='home'>
-            <div>Welcome!</div>
+            <div className={isScrolled ? 'scrolled' : 'title'}>Welcome!</div>
         </div>
     )
 }
