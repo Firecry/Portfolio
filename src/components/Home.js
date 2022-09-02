bcda914df09d4f3ad0c6169492cbee5eab29638f
Firecry <lubiexd80@gmail.com>
2022-09-02 11:57:33 +0200

    updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,26 @@
 import '../css/Home.css'
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
 
-    
+    const [offsetY, setOffsetY] = useState(0);
+    const handleScroll = () => setOffsetY(window.pageYOffset);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
 
     return (
         <div className='home' id='home'>
-            <div className='sun'></div>
+            <div className='sun' style={{ transform: `translate(${offsetY * .8}px, ${-offsetY * .3}px)` }}></div>
             <div className='cloud1'></div>
             <div className='cloud2'></div>
             <div className='hill'></div>
             <div className='hillShadow'></div>
-            <div className='typing-demo'>
-                Hello I'm <div className='name'> Simon </div> Web Developer.
+            <div className='typing-layout'>
+                <div className='typing-demo'>Hello I'm <div className='name'> Simon </div> Web Developer.</div>
             </div>
         </div>
     )
