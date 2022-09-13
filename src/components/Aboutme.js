3196e3bdb6501562ebeeabf8f0b90bff1b0e4cbb
Firecry <lubiexd80@gmail.com>
2022-09-13 20:38:14 +0200

    updates

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,9 +1,24 @@
 import '../css/Aboutme.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
 
 export default function Aboutme() {
     return(
         <div className='aboutme' id='aboutme'>
-            {/* <div className='description'>Hello World!</div> */}
+            <div
+                className='description'
+                data-aos='fade-right'
+                data-aos-duration="1000"
+            >
+                Hello World!
+            </div>
+            <div
+                className='photo'
+                data-aos='fade-left'
+                data-aos-duration="1000"
+            ></div>
         </div>
     )
 }
