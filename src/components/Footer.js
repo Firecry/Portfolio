ebed35eff1ac3018a565d19840276b825938a9ea
Firecry <lubiexd80@gmail.com>
2022-09-17 02:51:33 +0200

    updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import '../css/Footer.css'
 export default function Footer() {
     return (
         <div className='footer'>
-            
+            <a href="https://github.com/Firecry" target="_blank" rel="noreferrer"><div className='github'></div></a>
         </div>
     )
 }
