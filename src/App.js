93d905bc1fd3c85bfdb0427012983492850f02b8
Firecry <lubiexd80@gmail.com>
2022-10-20 05:51:19 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
   return (
     <div className='body'>
       <Navbar />
-      <Home />
-      <Aboutme />
-      <Projects />
-      <Hobby />
+        <Home />
+        <Aboutme />
+        <Projects />
+        <Hobby />
       <Footer />
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import '../css/Footer.css'
 export default function Footer() {
     return (
         <div className='footer'>
-            <a href="https://github.com/Firecry" target="_blank" rel="noreferrer"><div className='github'></div></a>
+            <a href="https://github.com/Firecry" target="_blank" rel="noreferrer"><div className='github footer-items'></div></a>
+            <a href="https://www.facebook.com/szy.milel.7/" target="_blank" rel="noreferrer"><div className='facebook footer-items'></div></a>
+            <a href="https://www.instagram.com/szymonsmolorz/" target="_blank" rel="noreferrer"><div className='instagram footer-items'></div></a>
+            <a href="mailto:szymon.smolorz@gmail.com" target="_blank" rel="noreferrer"><div className='mail footer-items'></div></a>
         </div>
     )
 }
