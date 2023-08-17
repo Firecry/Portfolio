24249502ff348756fefb30ef5c7e43063fdb3a50
Firecry <lubiexd80@gmail.com>
2023-08-17 15:36:24 +0200

    contact page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,8 @@ import Home from './components/Home';
 import Footer from './components/Footer';
 import Aboutme from './components/Aboutme';
 import Projects from './components/Projects';
-<<<<<<< HEAD
-import Hobby from './components/Hobby';
-import Contact from './components/Contact';
-import './css/App.css';
-=======
 import Hobby from './components/Hobby'
-import './scss/App.scss';
->>>>>>> fbb78da2113dc77e7fd73e966785c5bbeb956edb
+import './css/App.css';
 
 function App() {
   return (
