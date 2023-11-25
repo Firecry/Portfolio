a63e1287b3512299359560b4ec785a4b561e5664
Firecry <lubiexd80@gmail.com>
2023-11-25 23:11:51 +0100

    ladybugs

--- src/hooks/Canvas.js
+++ src/hooks/Canvas.js
@@ -7,14 +7,15 @@ export function Canvas(props){
         const canvas = canvasRef.current;
         const c = canvas.getContext('2d');
 
-        var ballAmount = 50;
+        var ballAmount = 100;
 
         var colorPalette = [
-            '#692400',
-            '#BDA79D',
-            '#87736A',
-            '#004607',
-            '#357736'
+            '#CDEC27',
+            '#87D832',
+            '#2EC241',
+            '#00AA50',
+            '#00905C',
+            '#007664'
         ];
 
         window.addEventListener('resize', ()=>{
@@ -24,45 +25,52 @@ export function Canvas(props){
             init();
         })
 
+        function randomIntFromRange(min, max){
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+
+        var lottery = [
+            'Math.cos(this.radians)',
+            'Math.sin(this.radians)'
+        ]
+
         // Animated Circle
-        function Circle(x, y, dx, dy, radius){
+        function Circle(x, y, radius){
             this.x = x;
             this.y = y;
-            this.dx = dx;
-            this.dy = dy;
             this.radius = radius;
             this.minRadius = radius;
-            this.mindx = dx;
-            this.mindy = dy;
             this.color = colorPalette[Math.floor(Math.random() * colorPalette.length)];
+            this.radians = 0;
+            this.velocity = Math.random() * 0.015 + 0.002;
+            this.winner = lottery[Math.floor(Math.random() * lottery.length)].value;
+            this.distance = {
+                x: randomIntFromRange(Math.floor(Math.random() * 120), Math.floor(Math.random() * 120)),
+                y: randomIntFromRange(Math.floor(Math.random() * 50), Math.floor(Math.random() * 80))
+            }
+
+            this.update = ()=>{
+                this.radians += this.velocity;
+                this.x = x + Math.sin(this.radians) * this.distance.x;
+                this.y = y + Math.cos(this.radians) * this.distance.y;
+                this.draw();
+            }
 
             this.draw = ()=>{
                 c.beginPath();
                 c.fillStyle = this.color;
                 c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
                 c.strokeStyle = '#222';
-                c.stroke();
+                // c.stroke();
                 c.fill();
-            }
-
-            this.update = ()=>{
-                if(this.x + this.radius > canvas.width || this.x - this.radius < 0){
-                    this.dx = -this.dx;
-                }
-                if(this.y + this.radius > canvas.height || this.y - this.radius < 0){
-                    this.dy = -this.dy;
-                }
-            
-                this.x += this.dx;
-                this.y += this.dy;
-
-                if(this.dy < 10){
-                    this.dy+=0.01;
-                }
-
-                this.draw();
+                c.shadowColor = '#CDEC27';
+                c.shadowBlur = 20;
+                c.shadowOffsetX = 0;
+                c.shadowOffsetY = 0;
             }
         }
+
+        // console.log(eval(lottery[Math.floor(Math.random() * lottery.length)]));
 
         var circleArray = []
 
@@ -71,16 +79,17 @@ export function Canvas(props){
 
             for(let i = 0; i < (ballAmount + 1); i++){
                 // eslint-disable-next-line
-                var x = Math.floor(Math.random() * (canvas.width - radius * 5) + radius);
+                var x = Math.floor(Math.random() * (canvas.width - radius * 20) + radius);
                 // eslint-disable-next-line
-                var y = Math.floor(Math.random() * (canvas.height - radius * 5) + radius);
-                var dx = (Math.random() - .5) * 5;
-                var dy = (Math.random() - .5);
-                var radius = Math.random() * 5 + 3;
+                var y = Math.floor(Math.random() * (canvas.height - radius * 40) + radius);
 
-                circleArray.push(new Circle(x, y, dx, dy, radius));
+                var radius = Math.random() * 3 + 1.5;
+                
+
+                circleArray.push(new Circle(x, y, radius));
             }
         }
+
 
         function animate(){
             requestAnimationFrame(animate);
