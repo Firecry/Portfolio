fc3ca69fc98d839c937398be2d2ded80659188df
Firecry <lubiexd80@gmail.com>
2024-11-27 05:13:56 +0100

    update

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -22,12 +22,12 @@ export default function Aboutme() {
                     data-aos-delay='300'
                 >
                     <p>
-                        I'm a frontend web developer, creating websites using React framework.
-                        I'm 21 years old and come from Poland. I used to study HTML and CSS for 4 years in IT school,
-                         but im still learning by myself. It's been 5 years at all.
-                        My interest in JavaScript started 2 years ago, by my friend.
-                        It was like he was telling me I had to start, but I didn't want to so much.
-                        But now I am fascinated and I want to learn new things every day.
+                    I'm a 22-year-old frontend web developer from Poland, specializing in creating modern websites with React.
+                    My journey began in IT school, where I studied HTML and CSS for four years, and over the past five years,
+                    I've dedicated myself to continuous learning.
+                    Three years ago, I discovered JavaScript, which sparked my passion for building dynamic, interactive web applications.
+                    I love combining code and design to craft intuitive user experiences while staying updated on the latest web technologies.
+                    My current goal is to expand into full-stack development, exploring backend tools like Node.js to broaden my expertise.
                     </p>
                 </div>
             <div
